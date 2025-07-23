e4979e4c2b53fdf15c578bc58ed93e809999760e
gkhep <gk.heptonstall@gmail.com>
2025-07-23 22:17:58 +0800

    add unit test

--- src/main/java/org/gkh/model/MelbourneWeatherDTO.java
+++ src/main/java/org/gkh/model/MelbourneWeatherDTO.java
@@ -4,10 +4,12 @@ import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import lombok.Builder;
 import lombok.Data;
+import lombok.ToString;
 
 @Data
 @Builder
 @JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
+@ToString
 public class MelbourneWeatherDTO {
 
     private String temperatureDegrees;
---
+++ src/test/java/org/gkh/controller/WeatherControllerTests.java
@@ -0,0 +1,40 @@
+package org.gkh.controller;
+
+import org.gkh.model.MelbourneWeatherDTO;
+import org.gkh.service.OpenWeatherMapService;
+import org.gkh.service.WeatherStackService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class WeatherControllerTests {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockitoBean
+    private WeatherStackService weatherstackService;
+
+    @MockitoBean
+    private OpenWeatherMapService openweathermapService;
+
+    @Test
+    void pingWeather_shouldReturnWeatherStackResponse() throws Exception {
+        MelbourneWeatherDTO expected = MelbourneWeatherDTO.builder()
+                .temperatureDegrees("20")
+                .windSpeed("10")
+                .build();
+        when(weatherstackService.getWeather()).thenReturn(expected);
+        this.mockMvc.perform(get("/ping")).andExpect(status().isOk()).andExpect(content().contentType("application/json"));
+    }
+}
